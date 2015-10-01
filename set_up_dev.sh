18d4f378679cde18a4e49d14dd357bdc39ac3510
Kevin Clark <kac8dd@virginia.edu>
2015-10-01 02:12:32 -0700

    added experience server and web front end

--- set_up_dev.sh
+++ set_up_dev.sh
@@ -2,8 +2,8 @@
 
 docker run -d --name models -p 8001:8000 -v /home/webmin/ISA_code/models_api:/app --link mysql:db_host tp33/django:devel mod_wsgi-express start-server models_api/wsgi.py 
 
-docker run -d --name exp -p 8001:8000 -v /home/webmin/ISA_code/models_api:/app --link models:models_host tp33/django:devel mod_wsgi-express start-server models_api/wsgi.py
+docker run -d --name exp -p 8001:8000 -v /home/webmin/ISA_code/exp_server_code:/app --link models:models_host tp33/django:devel mod_wsgi-express start-server models_api/wsgi.py
 
-docker run -d --name models -p 8001:8000 -v /home/webmin/ISA_code/models_api:/app --link exp:exp_host tp33/django:devel mod_wsgi-express start-server models_api/wsgi.py
+docker run -d --name models -p 8001:8000 -v /home/webmin/ISA_code/web_frontend_server_code:/app --link exp:exp_host tp33/django:devel mod_wsgi-express start-server models_api/wsgi.py
 
 
