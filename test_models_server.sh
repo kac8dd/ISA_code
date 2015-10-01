34d934e20276ac9fa451bfb9f5c0f4f5138be939
Kevin Clark <kac8dd@virginia.edu>
2015-10-01 01:07:00 -0700

    added explanation to server test script

--- test_models_server.sh
+++ test_models_server.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#############################################################
+## this script does the following for each type of model
+## 	create new object and save to the database 
+## 	get that object back from the database
+## 	update that object and save to the database
+## 	get updated version of that object back from the database
+#############
+## If one of the curl commands below is not working properly
+## 	append the command with " > response.html && firefox response.html" 
+##	then rerun the script and observe the debug output in firefox
+e############################################################# 
 curl localhost:8001/api/v1/create/event/ --data "name=test_event_1&description=test_description&start_time=2015-12-03&location=test_location"
  
 echo "";echo "" 
