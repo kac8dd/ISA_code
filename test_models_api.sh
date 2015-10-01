4e3aacb81eea7cbb11e5f605fa694450015a5b2f
Kevin Clark <kac8dd@virginia.edu>
2015-10-01 14:58:31 -0700

    fixed json responses

--- test_models_api.sh
+++ test_models_api.sh
@@ -10,12 +10,28 @@
 ## If one of the curl commands below is not working properly
 ## 	append the command with " > response.html && firefox response.html" 
 ##	then rerun the script and observe the debug output in firefox
-e############################################################# 
-curl localhost:8001/api/v1/create/event/ --data "name=test_event_1&description=test_description&start_time=2015-12-03&location=test_location"
+############################################################# 
+
+curl localhost:8001/api/v1/create/user/ --data "username=test_username&password=test_password&firstname=John&lastname=Doe" 
+echo "";echo "" 
+
+curl localhost:8001/api/v1/get/user/1/  
+echo "";echo ""
+
+curl localhost:8001/api/v1/update/user/1/ --data "password=updated_password&firstname=Jane&lastname=Philips"  
+echo "";echo ""
+
+curl localhost:8001/api/v1/get/user/1/
+echo "";echo ""
+
+
+curl localhost:8001/api/v1/create/event/ --data "name=test_event_1&description=test_description&start_time=2015-12-03&location=test_location&creator_id=1" 
+ 
  
 echo "";echo "" 
 
-curl localhost:8001/api/v1/get/event/1/
+curl localhost:8001/api/v1/get/event/1/ 
+ 
 echo "";echo "" 
 
 curl localhost:8001/api/v1/update/event/1/ --data "name=updated_name&description=updated_description&start_time=2050-12-03&location=updated_location" 
@@ -34,20 +50,6 @@ curl localhost:8001/api/v1/update/ticket/1/ --data "name=updated_ticket_name&pri
 echo "";echo ""
 
 curl localhost:8001/api/v1/get/ticket/1/
-echo "";echo ""
-
-curl localhost:8001/api/v1/create/user/ --data "username=test_username&password=test_password&firstname=John&lastname=Doe"  
-echo "";echo ""
-
-curl localhost:8001/api/v1/get/user/1/ 
-echo "";echo ""
-
-curl localhost:8001/api/v1/update/user/1/ --data "password=updated_password&firstname=Jane&lastname=Philips" 
- 
- 
-echo "";echo ""
-
-curl localhost:8001/api/v1/get/user/1/
 echo "";echo ""
 
 curl localhost:8001/api/v1/create/purchase/ --data "user_id=1&ticket_id=1" 
--- tty_models.sh
+++ tty_models.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --name models -p 8001:8000 -v /home/webmin/ISA_code/exp_server_code:/app --link mysql:mysql_host tp33/django:devel python manage.py runserver 0.0.0.0:8000 
+docker run -it --name models -p 8001:8000 -v /home/webmin/ISA_code/models_server_code:/app --link mysql:db_host tp33/django:devel 
