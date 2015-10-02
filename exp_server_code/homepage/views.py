d128ce1edc5b4c76f4478b5f1c72bed8f946366d
Kevin Clark <kac8dd@virginia.edu>
2015-10-01 20:11:17 -0700

    stable version of homepage service

--- exp_server_code/homepage/views.py
+++ exp_server_code/homepage/views.py
@@ -15,16 +15,35 @@ from django.contrib.auth import hashers
 import urllib.request 
 from urllib.error import HTTPError
 
+import json
 
 #these are not front faceing POSTs so we don't need csrf tokens
 from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
+	num = 5	
 	try:
-		with urllib.request.urlopen("http://models_host:8000/api/v1/get/latest/5/") as url:
-			latest_events_json = url.read()
+		#make api call to models layer for the latest events
+		with urllib.request.urlopen("http://models_host:8000/api/v1/get/latest/"+str(num)+"/") as url:
+			#decode the response
+			str_response = url.readall().decode('utf-8') 
+			
+			#convert into json/python dictionary
+			latest_events_json = json.loads(str_response) 
+			
+			#get the information about the creator of each event and craft a response	
+			x = 0
+			response = {}
+	
+			while x < num:
+				current_event = latest_events_json[str(x)] 
+				str_response = urllib.request.urlopen("http://models_host:8000/api/v1/get/user/"+str(current_event['creator'])+"/").readall().decode('utf-8') 
+				current_creator= json.loads(str_response)
+				response[str(current_event)] = str(current_creator)
+				x=x+1	
 	except HTTPError:
 		return _error_response(request, 'unable to get http response from database')
-	return HttpResponse(latest_events_json)
+
+	return JsonResponse(response)
 
